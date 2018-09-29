5e0c475ec9c92292976f5d6102b67a8ffa7082d9
mukeshgoraigs <mukeshgorai24@gmail.com>
2018-09-29 20:19:17 +0530

    addition a page of connection

---
+++ html/connection.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$password=$_POST['password'];
+
+$sql = "INSERT INTO base (name,email,phone,password)
+VALUES ('$name', '$email', '$phone','$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
--- html/index.php
+++ html/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
@@ -11,7 +12,7 @@
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
 </head>
-
+<body>
 <div class="container-fluid ">
     <div class="row top-bar" style="height: 34px!important;">
         <div class="col-md-6 offset-6">
@@ -65,9 +66,81 @@
     <div class="row">
         <div class="col-md-12 heading">
             <h2 style="margin-top: 510px">Welcome to Mukesh Coder</h2>
-</div>
 
 </div>
+    </div>
+      <div class="row">
+        <div class="col-md-6">
+        <form action="connection.php" method="post">
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="text" class="form-control" name="email">
+        </div>
+        <div class="form-group">
+            <label>Mobile no</label>
+            <input type="text" class="form-control" name="phone">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input type="text" class="form-control" name="password">
+        </div>
+        <div class="form-group">
+            <button class="btn" type="submit" name="submit" >Save</button>
+        </div>
+        </form>
+        </div>
+          <div class="col-md-6">
+              <table class="table table-striped">
+                  <tr>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Mobile No.</th>
+                      <th>Password</th>
+                  </tr>
+              <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "data";
+
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if ($conn->connect_error) {
+                  die("Connection failed: " . $conn->connect_error);
+                  }
+                  else{
+
+      echo "connection created";
+
+                  }
+$sql = 'SELECT * FROM base';
+$query = mysqli_query($conn, $sql);
+                   while ($row = mysqli_fetch_array($query)) {
+                           $id = $row['id'];
+                           echo '<tr>
+                   					<td>' . $no . '</td>
+                   					<td>' . $row['name'] . '</td>
+
+                   					<td>' . $row['email'] . '</td>
+
+
+                   					<td><a href="delete.php?id='.$row['id'].'"><button type="button" class="btn btn-primary" >Delete</button></a></td>
+                   					<td><a href="update.php?id='.$row['id'].'"><button type="button" class="btn btn-primary" >Update</button></a></td>
+
+
+                   				</tr>';
+
+                  $conn->close();
+              ?>
+              </table>
+          </div>
+      </div>
+
 </div>
 </body>
 </html>
